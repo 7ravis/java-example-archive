59f92fc5c381ce14b0555a23fcdd88210383cac0
Travis Rogers <7ravis@gmail.com>
2017-11-09 12:51:28 -0600

    create project for examples

---
+++ examples/src/main/java/com/twrog/examples/ExampleMain.java
@@ -0,0 +1,13 @@
+package com.twrog.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class ExampleMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
