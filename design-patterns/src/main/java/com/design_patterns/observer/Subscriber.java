ab0c808b353bb02a23ed5055fd69251720a0ea48
Travis Rogers <7ravis@gmail.com>
2017-11-18 23:13:51 -0600

    add design-patterns project and observer example

---
+++ design-patterns/src/main/java/com/design_patterns/observer/Subscriber.java
@@ -0,0 +1,23 @@
+package com.design_patterns.observer;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Subscriber implements Observer {
+    private String subscriberName;
+
+    public Subscriber(String subscriberName) {
+	this.subscriberName = subscriberName;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+	System.out.println(
+		subscriberName + ": new episode available from " + ((Podcast) o).getPodcastName() + ": " + arg);
+    }
+
+    public void subscribe(Observable o) {
+	o.addObserver(this);
+    }
+
+}
