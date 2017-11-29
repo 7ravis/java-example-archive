b96bd85ae01ead8f0aec6e0fd45cc82d6eb4e4d3
Travis Rogers <7ravis@gmail.com>
2017-11-29 12:00:36 -0600

    add example of spring multi-module project poms

---
+++ multi-module/application/src/main/java/application/app.java
@@ -0,0 +1,10 @@
+package application;
+
+public class app {
+
+    public static void main(String[] args) {
+	// FIXME unimplemented method
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
